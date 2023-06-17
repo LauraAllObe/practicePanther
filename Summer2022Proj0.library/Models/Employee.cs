287d3883c42488d85622cb191f1ef0546db4d29e
LauraAllObe <lauraaobermaier@gmail.com>
2023-06-17 13:38:33 -0400

    added employee class closes #11

---
+++ Summer2022Proj0.library/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summer2022Proj0.library.Models
+{
+    public class Employee
+    {
+        public string? Name { get; set; }
+        public int Id { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
