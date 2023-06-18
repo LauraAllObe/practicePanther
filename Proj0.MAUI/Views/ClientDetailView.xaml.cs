bc7176be104261be545d193ed9c24945a7c77f38
LauraAllObe <lauraaobermaier@gmail.com>
2023-06-18 14:55:15 -0400

    implemented functionality used to update a client's contents excluding projects closes #3

--- Proj0.MAUI/Views/ClientDetailView.xaml.cs
+++ Proj0.MAUI/Views/ClientDetailView.xaml.cs
@@ -1,18 +1,25 @@
 using Proj0.MAUI.ViewModels;
 
 namespace Proj0.MAUI.Views;
-
+[QueryProperty(nameof(ClientId), "clientId")]
 public partial class ClientDetailView : ContentPage
 {
+    public int ClientId { get; set; }
     public ClientDetailView()
     {
         InitializeComponent();
-        BindingContext = new ClientDetailViewModel();
+        if (ClientId > 0)
+            BindingContext = new ClientDetailViewModel(ClientId);
+        else
+            BindingContext = new ClientDetailViewModel();
     }
 
     private void OkClicked(object sender, EventArgs e)
     {
-        (BindingContext as ClientDetailViewModel).Add();
+        if(!(ClientId > 0))
+            (BindingContext as ClientDetailViewModel).Add();
+        else
+            (BindingContext as ClientDetailViewModel).Edit();
         Shell.Current.GoToAsync("//Clients");
     }
 
@@ -25,4 +32,15 @@ public partial class ClientDetailView : ContentPage
     {
         (BindingContext as ClientDetailViewModel).Active(false);
     }
+
+    private void OnLeaving(object sender, NavigatedFromEventArgs e)
+    {
+        BindingContext = null;
+    }
+
+    private void OnArriving(object sender, NavigatedToEventArgs e)
+    {
+        BindingContext = new ClientDetailViewModel(ClientId);
+    }
 }
+
